fff0077e7a68242ca967dc58c07861dea5434b3e
Santiago Lacayo (NYU) <santiagolacayonyu@10-23-1-3.dynapool.nyu.edu>
2025-03-17 22:31:50 -0400

    midterm

--- Mid Term Project/js.js
+++ Mid Term Project/js.js
@@ -13,3 +13,21 @@ function toggleImages() {
        img1.classList.remove("spin");
     }
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
