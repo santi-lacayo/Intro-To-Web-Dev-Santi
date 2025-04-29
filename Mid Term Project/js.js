c4db45d44a49e27fc617a6fe28ddb63b96ea6bd4
Santiago Lacayo (NYU) <santiagolacayonyu@10-18-173-61.dynapool.wireless.nyu.edu>
2025-04-29 10:01:09 -0400

    process

--- Mid Term Project/js.js
+++ Mid Term Project/js.js
@@ -1,18 +1,24 @@
 //@sd5056@nyu.edu helped me with this part of the code
-function toggleImages() {
-    const img1 = document.getElementById("image1");
-    const img2 = document.getElementById("image2"); 
-    if (img1.classList.contains("hidden")) {
-       img1.classList.remove("hidden");
-       img1.classList.add("spin");
-       img2.classList.add("hidden");
-       img2.classList.remove("spin");
-    } else {
-       img2.classList.remove("hidden");
-       img2.classList.add("spin");
-       img1.classList.add("hidden");
-       img1.classList.remove("spin");
-    }
+    var images = [
+      document.getElementById("image1"),
+      document.getElementById("image2"),
+      document.getElementById("image3"), 
+      document.getElementById("image4"), 
+      document.getElementById("image5")
+    ]; 
+
+    for(i = )
+
+    let current = 0;
+
+    function toggleImages() {
+      images[current].classList.add("hidden");
+      images[current].classList.remove("spin");
+
+    current = (current + 1) %images.length;
+
+      images[current].classList.remove("hidden");
+      images[current].classList.add("spin");
 }
 
 //the idea of this slideshow comes from: https://www.w3schools.com/w3css/w3css_slideshow.asp
@@ -25,7 +31,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("slideshow");
+  var x = document.getElementsByClassName("mySlides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
